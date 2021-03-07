526c4fa567babb146cf4db70fcc71ede3c061819
nicmolina <nicolasmarcondes99@gmail.com>
2021-03-07 18:25:59 -0300

    chang env process

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import routes from './routes';
 const app = express();
 
+require('dotenv').config()
+
 app.use(routes);
 
-app.listen(3333, ()=>{
-    console.log('Server started on port 3333!')
-});
+app.listen(process.env.PORT || 3333);
